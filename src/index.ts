d5f411cc91c6187d6a87e56af29895e714bef1f7
Carlos Manriquez <carlos@digitalpotential.se>
2024-10-02 23:54:11 +0200

    trailings slash fix?

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ const app = express();
 // CORS configuration
 const allowedOrigins =
   process.env.NODE_ENV === "production"
-    ? ["https://husman-frontend.vercel.app/"]
-    : ["http://localhost:5173", "https://husman-frontend.vercel.app/"];
+    ? ["https://husman-frontend.vercel.app"]
+    : ["http://localhost:5173", "https://husman-frontend.vercel.app"];
 
 const corsOptions: cors.CorsOptions = {
   origin: function (origin, callback) {
